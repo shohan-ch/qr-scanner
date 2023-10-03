bd0381b473b4b16dde854aa2c9b7dc9f22eacaa8
shohan-ch <shohan889@yahoo.com>
2023-10-04 00:37:56 +0600

    color effect

--- src/components/ColorPlate/ColorsPlateView.jsx
+++ src/components/ColorPlate/ColorsPlateView.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import colotPlateData from "../../data/colorPlate.json";
 
 const ColorsPlateView = () => {
-  console.log(colotPlateData);
+  const [selectedColor, setSelectedColor] = useState("color_1");
+  const [colorPicker, setColorPicker] = useState({});
+
+  useEffect(() => {
+    const colorPickup = colotPlateData.find((color) => {
+      return selectedColor.split("_")[1] == color.id;
+    });
+    console.log(colorPickup, "effect");
+    setColorPicker(colorPickup);
+  }, [selectedColor]);
+
+  // const colorPickup = colotPlateData.find((color) => {
+  //   return selectedColor.split("_")[1] == color.id;
+  // });
+
+  const handleChangeColorPicker = (id, type) => (e) => {
+    console.log(type, e.target.value);
+    console.log(colorPicker, "change before");
+    setColorPicker((colorPicker) => ({ ...colorPicker, fgfd: e.target.value }));
+
+    console.log(colorPicker, "onchange after");
+    alert(type + id.toString());
+  };
+
+  // console.log(colorPicker, "dgfd");
 
   return (
     <>
@@ -20,6 +45,7 @@ const ColorsPlateView = () => {
                   value={"color_" + color.id}
                   defaultChecked=""
                   name="templateRadio"
+                  onChange={(e) => setSelectedColor(e.target.value)}
                 />
                 <label
                   htmlFor={"color_" + color.id}
@@ -46,11 +72,12 @@ const ColorsPlateView = () => {
             <p className="mb-2 text-xs font-semibold text-gray-500">Primary</p>
 
             <div className="flex justify-between px-4 py-3 bg-white rounded-3xl">
-              <p>#64ADD</p>
+              <p>{colorPicker?.primary}</p>
               <input
                 type="color"
-                value={"#ffff"}
+                value={colorPicker?.primary}
                 className="rounded cursor-pointer w-7"
+                onChange={handleChangeColorPicker(colorPicker.id, "primary")}
               />
             </div>
           </div>
@@ -61,11 +88,12 @@ const ColorsPlateView = () => {
             </p>
 
             <div className="flex justify-between px-4 py-3 bg-white rounded-3xl">
-              <p>#64ADD</p>
+              <p>{colorPicker?.secondary}</p>
               <input
                 type="color"
-                value={"#ffff"}
+                value={colorPicker?.secondary}
                 className="rounded cursor-pointer w-7"
+                onChange={handleChangeColorPicker(colorPicker.id, "secondary")}
               />
             </div>
           </div>
