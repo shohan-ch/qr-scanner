514c08a9e8d890f40f44511524edb7eb9b04b4dc
shohan-ch <shohan889@yahoo.com>
2023-11-07 00:02:27 +0600

    button design done

--- src/components/Location/LocationInputContainer.jsx
+++ src/components/Location/LocationInputContainer.jsx
@@ -1,61 +1,39 @@
 const LocationInputContainer = () => {
   return (
     <>
-      <div>
-        <input
-          type="radio"
-          name="locationButtonInput"
-          value={"complete"}
-          className="appearance-none sr-only peer"
-          id="radio1"
-        />
-        <label
-          htmlFor="radio1"
-          className="w-[100px] h-[50px] border-2 block peer-checked:border-red-400"
-        >
-          Complete
-        </label>
-      </div>
+      <div className="flex justify-center mt-4 gap-x-5">
+        <div>
+          <input
+            type="radio"
+            name="locationButtonInput"
+            value={"complete"}
+            className="appearance-none peer"
+            id="radio1"
+          />
+          <label
+            htmlFor="radio1"
+            className="p-5 w-[400px] text-center border-2 block peer-checked:border-blue-600 peer-checked:rounded-md"
+          >
+            Complete
+          </label>
+        </div>
 
-      <div>
-        <input
-          value={"url"}
-          type="radio"
-          name="locationButtonInput"
-          className="appearance-none sr-only peer"
-          id="radio2"
-        />
-        <label
-          htmlFor="radio2"
-          className="w-[100px] h-[50px] border-2 block peer-checked:border-red-400"
-        >
-          Url
-        </label>
+        <div>
+          <input
+            value={"url"}
+            type="radio"
+            name="locationButtonInput"
+            className="appearance-none peer"
+            id="radio2"
+          />
+          <label
+            htmlFor="radio2"
+            className="p-5 w-[400px] text-center border-2 block peer-checked:border-blue-600 peer-checked:rounded-md"
+          >
+            Url
+          </label>
+        </div>
       </div>
-
-      {/* <div>
-        <input
-          type="radio"
-          name="locationButtonInput"
-          classNameName="appearance-none"
-        />
-        <label
-          htmlFor="locationButtonInput"
-          classNameName="w-[100px] h-[50px] border border-red-400 block "
-        ></label>
-      </div>
-
-      <div>
-        <input
-          type="radio"
-          name="locationButtonInput"
-          classNameName="appearance-none"
-        />
-        <label
-          htmlFor="locationButtonInput"
-          classNameName="w-[100px] h-[50px] border border-red-400 block "
-        ></label>
-      </div>  */}
     </>
   );
 };
