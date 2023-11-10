ee446212cfae4ee9f2bcbf5231b89a81e7d3f3f0
shohan-ch <shohan889@yahoo.com>
2023-11-10 23:54:34 +0600

    location filter added

--- src/components/Location/CompleteLocationField.jsx
+++ src/components/Location/CompleteLocationField.jsx
@@ -1,17 +1,34 @@
 import InputField from "../Forms/InputField";
 import SelectField from "../Forms/SelectField";
 import Button from "../Utils/Button";
+import data from "../../data/LocationData.json";
+import { useState } from "react";
 
 const CompleteLocationField = () => {
+  const [location, setLocation] = useState("");
+  const handleLocationChange = (e) => {
+    setLocation(
+      data.filter((location, index) => location.street.match(e.target.value))
+    );
+  };
+
+  console.log(location);
   return (
     <>
       <div className="container px-2 mt-4">
         <div className="flex gap-x-3">
           <InputField
+            handleChange={handleLocationChange}
             placeholder="State / Province"
             name="name"
             className="w-full bg-white rounded-2xl"
           />
+
+          <div className="locationList">
+            <button value={1}>1</button>
+            <button value={2}>2</button>
+          </div>
+
           <Button className="mt-2 text-[13px]  w-[250px] h-[40px]">
             {" "}
             Delete address{" "}
