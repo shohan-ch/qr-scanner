ee7dc9aae7f8259deac536cc9e4a80acb4cec68b
shohan-ch <shohan889@yahoo.com>
2023-11-22 22:28:56 +0600

    social networks initioally done

--- src/components/SoicialNetwork/SocialNetworkContainer.jsx
+++ src/components/SoicialNetwork/SocialNetworkContainer.jsx
@@ -4,8 +4,29 @@ import SocialNetworkList from "./SocialNetworkList";
 
 const SocialNetworkContainer = () => {
   const [socialList, setSocialList] = useState([]);
+  const [socialInfo, setSocialInfo] = useState([]);
 
-  console.log(socialList);
+  const handleSocialInfoChange = (socialName) => (e) => {
+    const { name, value } = e.target;
+    let info = socialInfo.map((social) =>
+      social.socialName == socialName ? { ...social, [name]: value } : social
+    );
+    let updateInfo = socialInfo.some(
+      (social) => social.socialName == socialName
+    )
+      ? info
+      : [...socialInfo, { socialName: socialName, [name]: value }];
+    setSocialInfo(updateInfo);
+  };
+
+  const handleSocialRemove = (socialName) => () => {
+    setSocialInfo(
+      socialInfo.filter((social) => social.socialName !== socialName)
+    );
+    setSocialList(socialList.filter((social) => social.name !== socialName));
+  };
+
+  console.log(socialInfo);
   return (
     <>
       <div className="mx-2 space-y-7 mt-7">
@@ -17,9 +38,25 @@ const SocialNetworkContainer = () => {
             <div className="flex items-center gap-x-5">
               <img src={social.logo} alt={social.name} width={30} />
               <p className="text-[16px]">{social.name}</p>
+              <button
+                className="ml-auto text-gray-600"
+                onClick={handleSocialRemove(social.name)}
+              >
+                X
+              </button>
             </div>
-            <InputField placeholder="Url" className="bg-white" />
-            <InputField placeholder="Text" className="bg-white " />
+            <InputField
+              handleChange={handleSocialInfoChange(social.name)}
+              name="url"
+              placeholder="Url"
+              className="bg-white"
+            />
+            <InputField
+              handleChange={handleSocialInfoChange(social.name)}
+              name="text"
+              placeholder="Text"
+              className="bg-white "
+            />
           </div>
         ))}
 
